01286d791a42277e62665310f88a4a8c549935d6
scul0405 <90521402+scul0405@users.noreply.github.com>
2022-08-03 22:14:22 +0700

    Fix port issue between client and server by add cors

--- server/app.js
+++ server/app.js
@@ -1,31 +1,30 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 const app = express();
-const userRouter = require('./routes/userRoutes')
-const todoRouter = require('./routes/todoRoutes')
-const globalErrorHandler = require('./controllers/errorController');
-const AppError = require('./utils/appError');
+const cors = require("cors");
+const userRouter = require("./routes/userRoutes");
+const todoRouter = require("./routes/todoRoutes");
+const globalErrorHandler = require("./controllers/errorController");
+const AppError = require("./utils/appError");
 
 // set environment
-if (process.env.NODE_ENV === 'development')
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 // middleware
-app.use(express.json())
-
+app.use(express.json());
+app.use(cors()); // fix port
 
 // Set default route
-app.use('/api/v1/users',userRouter)
-app.use('/api/v1/todos', todoRouter)
-
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/todos", todoRouter);
 
 // Handle when go to undefined route
-app.all('*',(req,res,next) => {
-   next(new AppError(404,`Can't find ${req.originalUrl} on this server !`))
-})
+app.all("*", (req, res, next) => {
+  next(new AppError(404, `Can't find ${req.originalUrl} on this server !`));
+});
 
 // Handle global error
 app.use(globalErrorHandler);
 
-module.exports = app
+module.exports = app;
