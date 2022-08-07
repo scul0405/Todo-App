a3fef297fa9851baac1de9eb1c38c457bb1d741b
scul0405 <90521402+scul0405@users.noreply.github.com>
2022-08-07 22:16:38 +0700

    make it better

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
@@ -14,26 +13,27 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors()); 
 
 // Set default route
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/todos", todoRouter);
 
+if (process.env.NODE_ENV === 'production'){
+  // serve the static files from react app
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  // Handles any requests that don't match the ones above
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(path.join(__dirname, "../client/build"), 'index.html'));
+  });
+}
 // Handle when go to undefined route
-// app.all("*", (req, res, next) => {
-//   next(new AppError(404, `Can't find ${req.originalUrl} on this server !`));
-// });
+app.all("*", (req, res, next) => {
+  next(new AppError(404, `Can't find ${req.originalUrl} on this server !`));
+});
+
 
 // Handle global error
 app.use(globalErrorHandler);
-
-// serve the static files from react app
-app.use(express.static(path.join(__dirname, "../client/build")));
-// Handles any requests that don't match the ones above
-app.get('*', (req, res) => {
-  res.sendFile(path.join(path.join(__dirname, "../client/build"), 'index.html'));
-}); 
-
 
 module.exports = app;
