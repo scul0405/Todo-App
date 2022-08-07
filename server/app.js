3a7a8c045bfee9a411bd8120efa37eeb40858149
scul0405 <90521402+scul0405@users.noreply.github.com>
2022-08-07 20:33:02 +0700

    server the static files from react app

--- server/app.js
+++ server/app.js
@@ -11,6 +11,9 @@ const AppError = require("./utils/appError");
 // set environment
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
+// serve the static files from react app
+app.use(express.static(path.join(__dirname, "../client/public")));
+
 // middleware
 app.use(express.json());
 // app.use(cors()); // fix port
