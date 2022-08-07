528281ab5c71120e94994327ea52b1c7919710d9
scul0405 <90521402+scul0405@users.noreply.github.com>
2022-08-07 19:08:43 +0700

    Implemented cors

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 // middleware
 app.use(express.json());
 app.use(cors()); // fix port
+app.options('*',cors())
 
 // Set default route
 app.use("/api/v1/users", userRouter);
